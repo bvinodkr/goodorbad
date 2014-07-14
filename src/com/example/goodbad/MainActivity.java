f06144a21747854cbe1d4c73ecf7966583203a6a
anurag16 <anurag.gupta.usc@gmail.com>
2014-07-14 13:22:11 -0700

    Home Screen, Fragments, Layouts etc

--- src/com/example/goodbad/MainActivity.java
+++ src/com/example/goodbad/MainActivity.java
@@ -1,47 +1,199 @@
 package com.example.goodbad;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
-import com.example.goodbad.fragments.HomeListFragment;
-import com.example.goodbad.fragments.MyListFragment;
+import com.example.goodbad.fragments.ComposeStoryFragment;
+import com.example.goodbad.fragments.ComposeStoryFragment.ComposeStoryFragmentListener;
+import com.example.goodbad.fragments.MyStoryListFragment;
+import com.example.goodbad.fragments.TrendingStoryListFragment;
 import com.example.goodbad.listeners.FragmentTabListener;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends ActionBarActivity implements ComposeStoryFragmentListener {
+
+	private ArrayList<PopUpWindowItem> popUpWindowItemList = new ArrayList<PopUpWindowItem>();
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//setBehindContentView(R.layout.activity_main);
+		setupTabs ();
+
+		//		SlidingMenu sm = getSlidingMenu();
+		//		sm.setShadowWidthRes(R.dimen.shadow_width);
+		//		sm.setShadowDrawable(R.drawable.shadow);
+		//		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		//		sm.setFadeDegree(0.35f);
+		//		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+	}
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_timeline);
-		setupTabs ();
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.action_bar_items, menu);
+
+		/*MenuItem miActionBarShareIcon = menu.findItem(R.id.miActionBarShareIcon);
+		ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(miActionBarShareIcon);
+		Log.d("debug ", "is null " + shareActionProvider);
+        shareActionProvider.setShareIntent(getDefaultShareIntent());*/
+
+		return super.onCreateOptionsMenu(menu);
 	}
 	
-	private void setupTabs() {
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.miPopUpIcon:
+				onPopUpIconCick(item);
+				return true;
+			case R.id.miActionBarComposeIcon:
+				onComposeIconClick(item);
+				return true;
+			case R.id.miPostStoryIcon:
+				// Not implemented here
+				return false;
+			default:
+				break;
+		}
+
+	    return super.onOptionsItemSelected(item);
+	}
+
+
+	public void onPopUpIconCick(MenuItem mi) {
+
+		// Inflate the popup_layout.xml		
+		View popUpView = getLayoutInflater().inflate(R.layout.pop_up_layout, null);
+
+		// Creating the PopupWindow
+		final PopupWindow popupWindow = new PopupWindow(this);
+		popupWindow.setContentView(popUpView);
+		popupWindow.setWidth(330);
+		popupWindow.setHeight(170);
+		popupWindow.setOutsideTouchable(true);
+		popupWindow.setFocusable(true);
+
+		/*// Clear the default translucent background
+		popupWindow.setBackgroundDrawable(new BitmapDrawable());
+		popupWindow.showAsDropDown(getCurrentFocus());*/
+		
+
+		// Displaying the popup at the specified location, + offsets.
+		popupWindow.showAtLocation(popUpView,  Gravity.NO_GRAVITY, 100, 110);
+		
+		populatePopUpWindowItems();
+
+		GridView gridview = (GridView) popUpView.findViewById(R.id.gvPopUp);		
+		gridview.setAdapter(new PopUpImageAdapter(this, popUpWindowItemList));
+
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+				Toast.makeText(getBaseContext(), "" + position, Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		/*View miPopUp = findViewById(R.id.miPopUp); // SAME ID AS MENU ID
+	    PopupMenu popupMenu = new PopupMenu(this, miPopUp); 
+	    popupMenu.getMenuInflater().inflate(R.menu.popup_items, popupMenu.getMenu());
+	    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+	        public boolean onMenuItemClick(MenuItem item) {
+	            switch (item.getItemId()) {
+	            case R.id.menu_keyword:
+	              Toast.makeText(MainActivity.this, "Keyword!", Toast.LENGTH_SHORT).show();
+	              return true;
+	            case R.id.menu_popularity:
+	              Toast.makeText(MainActivity.this, "Popularity!", Toast.LENGTH_SHORT).show();
+	              return true;
+	            default:
+	              return false;
+	            }
+	        }
+	    });
+	    popupMenu.show();*/
+	}
+	
+	private void populatePopUpWindowItems() {
+		popUpWindowItemList.clear();
+		popUpWindowItemList.add(new PopUpWindowItem("Gallery", R.drawable.gallery));
+		popUpWindowItemList.add(new PopUpWindowItem("Photo", R.drawable.camera));
+		popUpWindowItemList.add(new PopUpWindowItem("Video", R.drawable.video));
+		popUpWindowItemList.add(new PopUpWindowItem("Audio", R.drawable.voice));
+		popUpWindowItemList.add(new PopUpWindowItem("TBD", R.drawable.voice));
+		popUpWindowItemList.add(new PopUpWindowItem("TBD", R.drawable.voice));
+	}
+
+	/*private Intent getDefaultShareIntent(){
+    Intent intent = new Intent(Intent.ACTION_SEND);       
+    intent.setType("image/*");
+    return intent;
+	}*/
+
+	
+
+	public void onComposeIconClick(MenuItem mi) {
+		launchComposeDialog();
+	} 
+
+	private void launchComposeDialog() {
+		FragmentManager fm = getSupportFragmentManager();
+
+		ComposeStoryFragment composeStoryFragment = ComposeStoryFragment.newInstance("Compose Story");
+		composeStoryFragment.show(fm, "fragment_compose_tweet");
+	}
+
+	private void setupTabs() { 
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("Good/Bad");
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		actionBar.setDisplayShowTitleEnabled(true);
 
 		Tab tab1 = actionBar
-		    .newTab()
-		    .setText("Global")
-		    .setIcon(R.drawable.ic_global)
-		    .setTag("GlobalFragment")
-		    .setTabListener(new FragmentTabListener<HomeListFragment>(R.id.flContainer, this,
-                        "home", HomeListFragment.class));
+				.newTab()
+				.setText("Trending")
+				.setIcon(R.drawable.ic_global)
+				.setTag("GlobalFragment")
+				.setTabListener(new FragmentTabListener<TrendingStoryListFragment>(R.id.flContainer, this,
+						"home", TrendingStoryListFragment.class));
 
 		actionBar.addTab(tab1);
 		actionBar.selectTab(tab1);
 
 		Tab tab2 = actionBar
-		    .newTab()
-		    .setText("Favorite")
-		    .setIcon(R.drawable.ic_favorite)
-		    .setTag("FavoriteFragment")
-		    .setTabListener(new FragmentTabListener<MyListFragment>(R.id.flContainer, this,
-                        "mylist", MyListFragment.class));
+				.newTab()
+				.setText("Favorites")
+				.setIcon(R.drawable.ic_favorite)
+				.setTag("FavoriteFragment")
+				.setTabListener(new FragmentTabListener<MyStoryListFragment>(R.id.flContainer, this,
+						"mylist", MyStoryListFragment.class));
 		actionBar.addTab(tab2);
+
+		/*Tab tab3 = actionBar
+			    .newTab()
+			    .setText("New")
+			    .setIcon(R.drawable.ic_favorite)
+			    .setTag("FavoriteFragment")
+			    .setTabListener(new FragmentTabListener<MyStoryListFragment>(R.id.flContainer, this,
+	                        "mylist", MyStoryListFragment.class));
+			actionBar.addTab(tab3); */
+	}
+
+	@Override
+	public void onFinishComposeDialog() {
+		// TODO Auto-generated method stub
+
 	}
 }
--- src/com/example/goodbad/TreeNodeArrayAdapter.java
+++ src/com/example/goodbad/TreeNodeArrayAdapter.java
@@ -3,61 +3,108 @@ package com.example.goodbad;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 public class TreeNodeArrayAdapter extends ArrayAdapter<TreeNode> {
 
-	public TreeNodeArrayAdapter(Context context, List<TreeNode> objects) {
-		super(context, 0, objects);
+	public TreeNodeArrayAdapter(Context context, List<TreeNode> treeNodes) {
+		super(context, R.layout.treenode_item, treeNodes);
 		
 	}
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		TreeNode node = getItem (position);
-		View v;
 		if (convertView == null)
 		{
 			LayoutInflater inflator = LayoutInflater.from(getContext());
-			v = inflator.inflate(R.layout.treenode_item, parent, false);
-		}
-		else
-		{
-			v = convertView;
-		}
+			convertView = inflator.inflate(R.layout.treenode_item, parent, false);
+		} 
 	
-		ImageView ivProfileImage = (ImageView)v.findViewById(R.id.ivProfileImage);
-//		ivProfileImage.setTag(tweet.getUser().getScreenName());
-		/*
-		ivProfileImage.setOnClickListener(new OnClickListener() {
+		/*RelativeLayout rlTreeNodeItem = (RelativeLayout) convertView.findViewById(R.id.rlTreeNodeItem);	
+		RelativeLayout rlTreeNodeItemBody = (RelativeLayout) convertView.findViewById(R.id.rlBody);	
+		if(position==1) {
+
+			rlTreeNodeItem.getLayoutParams().height = 250;
+			rlTreeNodeItemBody.getLayoutParams().height = 185;
 			
-			@Override
-			public void onClick(View imageView) {
+			RelativeLayout.LayoutParams lpImageView = new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT, 190);
+			
+			ImageView imageView = new ImageView(getContext());
+			imageView.setImageResource(R.drawable.sample_0);
+			imageView.setId(2);
+			imageView.setPadding(0, 2, 0, 5);
+			lpImageView.addRule(RelativeLayout.BELOW, convertView.findViewById(R.id.tvBody).getId());
+			imageView.setLayoutParams(lpImageView);
+			rlTreeNodeItemBody.addView(imageView);
+		}*/
+		
+		ImageView ivItemImage = (ImageView) convertView.findViewById(R.id.ivItemImage);
+		if(position==1) { 			
+			ivItemImage.setVisibility(View.VISIBLE);
+			ivItemImage.setImageResource(R.drawable.yosemite);
+		} else if (position==5) { 			
+				ivItemImage.setVisibility(View.VISIBLE);
+				ivItemImage.setImageResource(R.drawable.football);			
+		} else {
+			ivItemImage.setVisibility(View.GONE);
+		}
 				
-//				Intent i = new Intent (getContext(), ProfileActivity.class); 
-//				i.putExtra("screenName", (String)imageView.getTag());
-//				imageView.getContext().startActivity (i);
+		 
+		//ImageView ivProfileImage = (ImageView)v.findViewById(R.id.ivProfileImage);
+		TextView tvUserName = (TextView) convertView.findViewById(R.id.tvUserName);
+		TextView tvBody = (TextView) convertView.findViewById(R.id.tvBody);
+		final TextView tvLikes = (TextView) convertView.findViewById(R.id.tvLikes);		
+		final ImageView ivEmptyHeart = (ImageView) convertView.findViewById(R.id.ivEmptyHeart);
+		ivEmptyHeart.setContentDescription("false");
+		
+		ImageLoader imageLoader = ImageLoader.getInstance();
+		
+		ivEmptyHeart.setOnClickListener(new OnClickListener() {
+			
+			@Override 
+			public void onClick(View v) {
+				int likes = Integer.parseInt(tvLikes.getText().toString());
+				boolean isLiked = Boolean.parseBoolean(ivEmptyHeart.getContentDescription().toString());
+				if(isLiked) {
+					if(likes > 0) { 
+						likes--;
+						ivEmptyHeart.setContentDescription("false");
+						ivEmptyHeart.setImageResource(R.drawable.ic_action_empty_heart);
+						tvLikes.setText(likes+"");
+					}
+				} else {
+					likes++;
+					ivEmptyHeart.setContentDescription("true");				
+					ivEmptyHeart.setImageResource(R.drawable.ic_action_filled_heart);
+					tvLikes.setText(likes+"");
+				}
 			}
-		});
-		*/
-		TextView tvUserName = (TextView)v.findViewById(R.id.tvUserName);
-		TextView tvBody = (TextView)v.findViewById(R.id.tvBody);
-//		TextView tvRelativeTime = (TextView)v.findViewById(R.id.tvRelativeTime);
-		ivProfileImage.setImageResource(android.R.color.transparent);
+		}); 
+		
 		tvUserName.setText("Vinod");
 		tvBody.setText (node.getText());
-		if (position%2 == 0)
-		{
-			v.setBackgroundColor(v.getResources().getColor (android.R.color.holo_orange_light));
-		}
-//		tvRelativeTime.setText("5 m");
-		return v;
+		
+		tvBody.setVisibility(0);
+		
+		Log.d("debug", node.getText()); 
+		/*if (position%2 == 0) {
+			convertView.setBackgroundColor(0xFF99CCFF);
+		} */
+		
+		return convertView;
 	}
 
 	
